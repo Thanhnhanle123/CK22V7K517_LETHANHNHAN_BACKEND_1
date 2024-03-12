8e4e65baa0ec13b44fdc447eb9150ec14dff2f95
Thanhnhanle123 <nestalect65@gmail.com>
2024-03-12 22:08:58 +0700

    Revert "backend 2"
    This reverts commit 10edcda6592a69587b0996af7d7be9f8fe1c088d.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
     node: true,
+    commonjs: true,
+    es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
+  extends: ["eslint:recommended", "prettier"],
 };
